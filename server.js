a22da8d4aa43911fb21cbf33ca1a6a7aa56f9bda
FONTANA Claudio 73426 <claudio.fontana@consulenti.csi.it>
2021-01-15 17:18:48 +0100

    feat: external parameter

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-docker logout
+require('dotenv').config({path : '\process.env'});
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  const favoriteFood = process.env.FAVORITE_FOOD;
   while(true) {
     console.log('Containers rule!');
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
